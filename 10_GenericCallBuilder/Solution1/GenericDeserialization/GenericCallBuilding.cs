e76143479d6ed9766aac85ac0210e05046a6452e
Sven Hübner <sven.huebner.it@gmx.de>
2023-11-30 19:51:32 +0100

    feat: add solution for puzzle 10 objective 1

---
+++ 10_GenericCallBuilder/Solution1/GenericDeserialization/GenericCallBuilding.cs
@@ -0,0 +1,56 @@
+﻿namespace GenericDeserialization
+{
+    public interface ICallback1<TReturn>
+    {
+        TReturn Invoke<T1>();
+    }
+
+    public interface ICallback2<TReturn>
+    {
+        TReturn Invoke<T1, T2>();
+    }
+
+    public interface ICallback3<TReturn>
+    {
+        TReturn Invoke<T1, T2, T3>();
+    }
+
+    abstract class TypeArguments1
+    {
+        public static TypeArguments1 From<T>() => new Of<T>();
+        public abstract TypeArguments2 Add(TypeArguments1 typeArgument);
+        public abstract TReturn Invoke<TReturn>(ICallback1<TReturn> callback);
+
+        public sealed class Of<T1> : TypeArguments1, ICallback1<TypeArguments2>
+        {
+            public override TypeArguments2 Add(TypeArguments1 typeArgument) => typeArgument.Invoke(this);
+            public override TReturn Invoke<TReturn>(ICallback1<TReturn> callback) => callback.Invoke<T1>();
+
+            public TypeArguments2 Invoke<T2>() => new TypeArguments2.Of<T1, T2>();
+        }
+    }
+
+    abstract class TypeArguments2
+    {
+        public abstract TypeArguments3 Add(TypeArguments1 typeArgument);
+        public abstract TReturn Invoke<TReturn>(ICallback2<TReturn> callback);
+
+        public sealed class Of<T1, T2> : TypeArguments2, ICallback1<TypeArguments3>
+        {
+            public override TypeArguments3 Add(TypeArguments1 typeArgument) => typeArgument.Invoke(this);
+            public override TReturn Invoke<TReturn>(ICallback2<TReturn> callback) => callback.Invoke<T1, T2>();
+
+            TypeArguments3 ICallback1<TypeArguments3>.Invoke<T3>() => new TypeArguments3.Of<T1, T2, T3>();
+        }
+    }
+
+    abstract class TypeArguments3
+    {
+        public abstract TReturn Invoke<TReturn>(ICallback3<TReturn> callback);
+
+        public sealed class Of<T1, T2, T3> : TypeArguments3
+        {
+            public override TReturn Invoke<TReturn>(ICallback3<TReturn> callback) => callback.Invoke<T1, T2, T3>();
+        }
+    }
+}
---
+++ 10_GenericCallBuilder/Solution1/GenericDeserialization/PipelineDeserializer.cs
@@ -0,0 +1,36 @@
+﻿namespace GenericDeserialization
+{
+    sealed class PipelineDeserializer : ICallback3<Pipeline>
+    {
+        public Pipeline Deserialize(string sourceType, string tempType, string targetType)
+        {
+            var sourceTypeArgument = GetTypeArgument(sourceType);
+            var tempTypeArgument = GetTypeArgument(tempType);
+            var targetTypeArgument = GetTypeArgument(targetType);
+
+            var typeArguments = sourceTypeArgument
+                .Add(tempTypeArgument)
+                .Add(targetTypeArgument);
+
+            Pipeline pipeline = typeArguments.Invoke(this);
+            return pipeline;
+        }
+
+        static TypeArguments1 GetTypeArgument(string typeStr) =>
+            typeStr switch
+            {
+                "string" => TypeArguments1.From<string>(),
+                "int" => TypeArguments1.From<int>(),
+                "SomeType" => TypeArguments1.From<SomeComplexType>(),
+                _ => throw new ArgumentException("unknown type", nameof(typeStr))
+            };
+
+        public Pipeline Invoke<T1, T2, T3>() =>
+            new DefaultPipeline<T1, T2, T3>(
+                Sources.Create<T1>(),
+                Mappers.Create<T1, T2>(),
+                Manipulators.Create<T2>(),
+                Mappers.Create<T2, T3>(),
+                Sinks.Create<T3>());
+    }
+}
